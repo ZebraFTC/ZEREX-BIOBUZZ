c94200b56f72b156de21ef656eaade29a585e4a7
ZLT06 <zebolts30416@gmail.com>
2026-09-14 19:06:17 -0400

    basic drive code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/zerex/MecanumDrive.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode.zerex;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class MecanumDrive {
+    public static final double DRIVE_TO_POWER = 0.2;
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+    private int frontLeftPosition;
+    private int frontRightPosition;
+    private int backLeftPosition;
+    private int backRightPosition;
+    private double xPos;
+    private double yPos;
+
+    public MecanumDrive(DcMotor frontLeft,DcMotor frontRight, DcMotor backLeft, DcMotor backRight){
+        this.frontLeft = frontLeft;
+        this.frontRight = frontRight;
+        this.backLeft = backLeft;
+        this.backRight = backRight;
+
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        yPos=0;
+        xPos=0;
+        resetEncoders();
+
+    }
+
+    public void resetEncoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // This mode runs the motors using internal velocity tuning curves
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void teleDrive(){
+        //
+    }
+    public void targetDrive(int frontLeftTarget, int frontRightTarget, int backLeftTarget, int backRightTarget, double speed){
+        frontLeft.setTargetPosition(frontLeft.getCurrentPosition() + frontLeftTarget);
+        frontRight.setTargetPosition(frontRight.getCurrentPosition() + frontRightTarget);
+        backLeft.setTargetPosition(backLeft.getCurrentPosition() + backLeftTarget);
+        backRight.setTargetPosition(backRight.getCurrentPosition() + backRightTarget);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+
+    }
+
+    public boolean isBusy() {
+        return frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy() || backRight.isBusy();
+    }
+
+    public void stopMotors() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+}
