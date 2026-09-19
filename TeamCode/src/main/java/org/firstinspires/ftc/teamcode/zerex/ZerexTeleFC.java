199668c79da9d01291ccb76bc59da4eaa69b5eae
ZLT08 <zebracompetitionteams@gmail.com>
2026-09-19 13:31:44 -0400

    pow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/zerex/ZerexTeleFC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/zerex/ZerexTeleFC.java
@@ -19,26 +19,26 @@ public class ZerexTeleFC extends LinearOpMode {
     private boolean lastOptionsState = false;
     @Override
     public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
+// Declare our motors
+// Make sure your ID's match your configuration
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
+// Reverse the right side motors. This may be wrong for your setup.
+// If your robot moves backwards when commanded to go forwards,
+// reverse the left side instead.
+// See the note about this earlier on this page.
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        // Retrieve the IMU from the hardware map
+// Retrieve the IMU from the hardware map
         GoBildaPinpointDriver pinpoint = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
-        // Adjust the orientation parameters to match your robot
+// Adjust the orientation parameters to match your robot
 
 
 
-        pinpoint.setOffsets(10,  -169, DistanceUnit.MM);
+        pinpoint.setOffsets(10, -169, DistanceUnit.MM);
 
         pinpoint.resetPosAndIMU();
 
@@ -57,37 +57,38 @@ public class ZerexTeleFC extends LinearOpMode {
 
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+            double turn = gamepad1.right_stick_x;
 
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
+// This button choice was made so that it is hard to hit on accident,
+// it can be freely changed based on preference.
+// The equivalent button is start on Xbox-style controllers.
             if (gamepad1.options && !lastOptionsState) {
                 pinpoint.recalibrateIMU();
             }
             lastOptionsState = gamepad1.options;
 
-            double botHeading = pinpoint.getHeading(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator * speed;
-            double backLeftPower = (rotY - rotX + rx) / denominator * speed;
-            double frontRightPower = (rotY - rotX - rx) / denominator * speed;
-            double backRightPower = (rotY + rotX - rx) / denominator * speed;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
+            double angleY = pinpoint.getHeading(AngleUnit.RADIANS);
+            double xSpeed = Math.copySign(Math.pow(Math.abs(x * Math.cos(-angleY) - y * Math.sin(-angleY)), 2), x * Math.cos(-angleY) - y * Math.sin(-angleY));
+            double ySpeed = Math.copySign(Math.pow(Math.abs(x * Math.sin(-angleY) + y * Math.cos(-angleY)), 2), x * Math.sin(-angleY) + y * Math.cos(-angleY));
+            double frontLeft = ySpeed + xSpeed + turn;
+            double frontRight = ySpeed - xSpeed - turn;
+            double backLeft = ySpeed - xSpeed + turn;
+            double backRight = ySpeed + xSpeed - turn;
+// Denominator is the largest motor power (absolute value) or 1
+// This ensures all the powers maintain the same ratio,
+// but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(frontLeft), Math.max(Math.abs(frontRight),
+                    Math.max(Math.abs(backLeft), Math.abs(backRight))));
+            if (denominator > 1.0) {
+                frontLeft /= denominator;
+                frontRight /= denominator;
+                backLeft /= denominator;
+                backRight /= denominator;
+            }
+            frontLeftMotor.setPower(frontLeft*speed);
+            backLeftMotor.setPower(backLeft*speed);
+            frontRightMotor.setPower(frontRight*speed);
+            backRightMotor.setPower(backRight*speed);
 
         }
     }
